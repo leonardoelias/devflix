1860e7f22f55f44ee8f412d1635804a1ebadd36b
Leonardo Elias <leonardo.elias4@gmail.com>
2021-12-23 21:35:45 -0300

    adicionado dados do filme em destaque

--- src/pages/PaginaInicial/index.js
+++ src/pages/PaginaInicial/index.js
@@ -1,9 +1,20 @@
 import {Banner} from '../../components/banner'
 
+const imageFake = 'https://observatoriodocinema.uol.com.br/wp-content/uploads/2021/11/homem-aranha-3-1.jpg';
+
+const filmeDestaque = {
+  titulo: 'Homen Aranha',
+  pontos: '4',
+  data_lancamento: '2020',
+  temporadas: '1',
+  genero: 'Terror',
+  imagem: imageFake,
+}
+
 export function PaginaInicial() {
   return(
     <div className="pag-inicial">
-      <Banner />
+      <Banner data={filmeDestaque} />
     </div>
   )
 }
