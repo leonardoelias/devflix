c290594304f6275c3507111217e67ac60bb2848b
Leonardo Elias <leonardo@rain.us>
2021-12-27 22:34:19 -0300

    add logo the movie db

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,8 +1,14 @@
-import './footer.css'
+import "./footer.css";
 export function Footer() {
   return (
     <footer>
+      <img
+        width={32}
+        height="auto"
+        src="https://www.themoviedb.org/assets/2/v4/logos/v2/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.svg"
+        alt="the movie db"
+      />
       <p>Desenvolvido com carinho</p>
     </footer>
-  )
+  );
 }
